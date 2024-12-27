0497970f4758aa61efa4b52944aaaa2f2d2b5e3f
Hanna <100560594+Han-nahh@users.noreply.github.com>
2024-12-27 21:44:11 +0300

    Update Star.jsx

--- E-Commerce-Template-main/src/components/filter/Star.jsx
+++ E-Commerce-Template-main/src/components/filter/Star.jsx
@@ -1,4 +1,8 @@
-const FilterStar = (props) => {
+const FilterStar = ({ onRatingFilterChange }) => {
+  const handleRatingChange = (rating) => {
+    onRatingFilterChange(rating);
+  };
+
   return (
     <div className="card mb-3">
       <div
@@ -11,101 +15,27 @@ const FilterStar = (props) => {
         Customer Review
       </div>
       <div className="card-body show" id="filterStar">
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="flexRadioDefault"
-            id="flexRadioDefault5"
-          />
-          <label
-            className="form-check-label"
-            htmlFor="flexRadioDefault5"
-            aria-label="Star"
-          >
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="flexRadioDefault"
-            id="flexRadioDefault4"
-          />
-          <label
-            className="form-check-label"
-            htmlFor="flexRadioDefault4"
-            aria-label="Star"
-          >
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="flexRadioDefault"
-            id="flexRadioDefault3"
-          />
-          <label
-            className="form-check-label"
-            htmlFor="flexRadioDefault3"
-            aria-label="Star"
-          >
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="flexRadioDefault"
-            id="flexRadioDefault2"
-          />
-          <label
-            className="form-check-label"
-            htmlFor="flexRadioDefault2"
-            aria-label="Star"
-          >
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="flexRadioDefault"
-            id="flexRadioDefault1"
-          />
-          <label
-            className="form-check-label"
-            htmlFor="flexRadioDefault1"
-            aria-label="Star"
-          >
-            <i className="bi bi-star-fill text-warning me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-            <i className="bi bi-star-fill text-secondary me-1 mb-2" />
-          </label>
-        </div>
+        {[5, 4, 3, 2, 1].map((rating) => (
+          <div className="form-check" key={rating}>
+            <input
+              className="form-check-input"
+              type="radio"
+              name="rating"
+              id={`rating-${rating}`}
+              onChange={() => handleRatingChange(rating)}
+            />
+            <label className="form-check-label" htmlFor={`rating-${rating}`}>
+              {Array.from({ length: 5 }, (_, index) => (
+                <i
+                  key={index}
+                  className={`bi bi-star-fill ${
+                    index < rating ? "text-warning" : "text-secondary"
+                  } me-1 mb-2`}
+                />
+              ))}
+            </label>
+          </div>
+        ))}
       </div>
     </div>
   );
